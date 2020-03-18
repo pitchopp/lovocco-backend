fdb8b16c3248e9bbfed285e30018b1f87186285e
Goldinger <sghir.ma@gmail.com>
2020-03-18 16:31:37 +0100

    added dislike method

--- backend/models.py
+++ backend/models.py
@@ -23,7 +23,7 @@ class City(models.Model):
 
 
 def get_image_path(instance, filename):
-    return os.path.join('photos', instance.user.username, filename)
+    return os.path.join('photos', instance.lover.id, filename)
 
 
 class Lover(models.Model):
